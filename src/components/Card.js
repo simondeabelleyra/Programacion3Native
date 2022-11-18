27713c7a729b37b8c5a8b38cb08a1b7dcac84902
MarcosBordeu <mbordeu@udesa.edu.ar>
2022-11-18 13:03:03 -0300

    hicimos likes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,70 @@
 import {React, Component} from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { db, auth } from '../firebase/config';
+import firebase from "firebase";
+import { TouchableOpacity } from "react-native-web";
+import { FontAwesome, AntDesign } from '@expo/vector-icons';
+
 
 class Card extends Component{
     constructor(props){
         super(props);
         this.state = {
-            props: props
+            props: props,
+            cantidadDeLikes: this.props.data.data.likes.length,
+            miLike: false,
         }
     };
+
+
+    componentDidMount() {
+        if (this.props.data.data.likes.includes(auth.currentUser.email)) {
+            this.setState({
+                miLike: true
+            })
+        } 
+        
+    }
+
+
+    botonLike(){
+        if(this.state.miLike === true){
+            this.disLike()
+        } else{
+            this.likes()
+        }
+    }
+
+    likes() {
+        db.collection('posts')
+        .doc(this.props.data.id)
+        .update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+        })
+        .then(()=> 
+            this.setState({
+                cantidadDeLikes: this.state.cantidadDeLikes + 1,
+                miLike: true
+            })
+           )
+        .catch(error=>console.log(error))
+    }
+
+    disLike() {
+        db.collection('posts')
+        .doc(this.props.data.id)
+        .update({
+            likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+        })
+        .then(()=> 
+            this.setState({
+                cantidadDeLikes: this.state.cantidadDeLikes -1,
+                miLike: false
+            })
+           )
+        .catch(error=>console.log(error))
+    }
+
 
     render(){
         return(
@@ -18,7 +75,15 @@ class Card extends Component{
                     source={{uri: this.props.data.data.photo}}
                 />
                 <Text style={style.contenido}>{this.props.data.data.description}</Text>
-                <Text style={style.contenido}>{this.props.data.data.likes.length} likes</Text>
+                <TouchableOpacity onPress={()=>this.botonLike()}>
+                    {this.state.miLike === false?
+                    <AntDesign name="hearto" size={24} color="white" />
+                    
+                    : <AntDesign name="heart" size={24} color="white" />
+                }
+                    
+                </TouchableOpacity>
+                <Text style={style.contenido}>likes: {this.state.cantidadDeLikes} </Text>
                 <Text style={style.contenido}>{this.props.data.data.comments.length} comentarios</Text>
             </View>
         )
@@ -32,8 +97,7 @@ const style = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'rgb(150,150,150)',
         borderStyle: 'solid',
-        borderRadius: 8,
-        backgroundColor: 'rgb(0,0,0)'
+        borderRadius: 8
     },
     creador: {
         fontWeight: 600,
